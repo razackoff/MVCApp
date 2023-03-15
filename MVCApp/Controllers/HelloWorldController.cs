1843eaa3e2c01c10baece0f65f699f991ddfbb52
Firuz <razackoff@gmail.com>
2023-03-15 20:04:34 +0500

    Добавить вид Hello.

--- MVCApp/Controllers/HelloWorldController.cs
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -9,9 +9,9 @@ namespace MVCApp.Controllers
             return View();
         }
 
-        public string Hello()
+        public IActionResult Hello()
         {
-            return "Who's there?";
+            return View();
         }
     }
 }
