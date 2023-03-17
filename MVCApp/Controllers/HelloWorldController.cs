3699909a4b7e11f63c21b9d9bebafc0948cd6a98
Firuz <razackoff@gmail.com>
2023-03-17 21:48:56 +0500

    Сохранение данных в лист.

--- MVCApp/Controllers/HelloWorldController.cs
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -5,7 +5,7 @@ namespace MVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
-        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        private static  List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         { 
             return View(dogs);
